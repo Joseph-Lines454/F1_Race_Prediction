8df6bef0453f7f6a69f393d2755478ec32192c08
Joseph Lines <joelines194@gmail.com>
2026-01-30 16:06:19 +0000

    Weather Data has been added to each race

--- Deep_Learning/GetWeatherData.py
+++ Deep_Learning/GetWeatherData.py
@@ -32,53 +32,81 @@ def GetCircuitsAndDates():
   return arrayWeather
 
 def WriteWeatherDatatoCSV(WeatherData):
-   print("This is the weather data CSV")
-   #open csv file then write the data to it
-   OpenWeatherCSV = pl.read_csv("F1_Data/WeatherData.csv", separator=",", encoding="latin1", ignore_errors=True)
+  print("This is the weather data CSV")
+  #open csv file then write the data to it
+  #OpenWeatherCSV = pl.read_csv("F1_Data/WeatherData.csv", separator=",", encoding="latin1", ignore_errors=True)
 
-   WeatherData.write_csv("F1_Data/WeatherData.csv", separator=",")
+
+
+  #Add headers here
+  
+
+  DataFrame = pl.DataFrame(WeatherData, schema=None,infer_schema_length = 0)
+  DataFrame.columns = [
+    "raceId",
+    "tempmax",
+    "tempmin",
+    "temp",
+    "dew",
+    "humidity",
+    "precip",
+    "snow",
+    "snowdepth",
+    "windspeed",
+    "winddir",
+    "pressure",
+    "cloudcover",
+    "visibility"
+  ]
+  DataFrame.write_csv("F1_Data/WeatherData.csv", separator=",")
+
+def FillInMissingValues(WeatherData):
+  for row in WeatherData:
+    for value in row:
+      if value == None or isinstance(value,str) == False:
+        value = " "
+      
+  return WeatherData
+
+def JoinData():
+  WriteToDataSet = pl.read_csv("F1_Data/FinalDataFormat.csv", separator=",", encoding="latin1",null_values=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"], ignore_errors=True)
+  WeatherData = pl.read_csv("F1_Data/WeatherData.csv", separator=",", null_values="\\N", ignore_errors=True)
+  WriteToDataSet = WriteToDataSet.join(WeatherData, on =["raceId"], suffix ="_k")
+  WriteToDataSet.write_csv("F1_Data/FinalDataFormat.csv", separator=",")
 
 def GetWeatherForcast(FindWeather):
-
+    i = 0
     #WriteToDataSet = OpenCSV()
     WeatherDataArray = np.empty((0,14),dtype=object)
     for row in FindWeather:
        #we need to convert the date format from dd/mm/yyyy to yyyy-mm-dd because thats the only format that the API suppports
       newdate = datetime.datetime.strptime(str(row[1]),"%d/%m/%Y").strftime("%Y-%m-%d")
-      WeatherData = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline" + "/" + str(row[2]) + "," + str(row[3]) + "/" + str(newdate) + "T" + str(row[4]) + "?key=HAWRLDPJJULW8C79XWHQGZP2F")
-
-      #We can make it so that the query is added to the correct data based on 
-      #WeatherData = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/-37.8497,144.968/2012-03-18T6:00:00?key=HAWRLDPJJULW8C79XWHQGZP2F&#include=current")
+      
+      WeatherData = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline" + "/" + str(row[2]) + "," + str(row[3]) + "/" + str(newdate) + "T" + str(row[4]) + "?key=HAWRLDPJJULW8C79XWHQGZP2F&include=current")
+      #print(WeatherData)
+        #We can make it so that the query is added to the correct data based on 
+        #WeatherData = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/-37.8497,144.968/2012-03-18T6:00:00?key=HAWRLDPJJULW8C79XWHQGZP2F&include=current")
+        #print(WeatherData)
       WeatherData = json.loads(WeatherData.text)
-      #Do we join data here?
-      #np.append(WeatherData, FindWeather[0])
+        #Do we join data here?
+        #np.append(WeatherData, FindWeather[0])
+        
       
-      
-      #Pull the values we need from the JSON data - DONT Join JSON data make a new array ready to join
+
+        #Pull the values we need from the JSON data - DONT Join JSON data make a new array ready to join
       WeatherDataArray = np.vstack([WeatherDataArray,[row[0],WeatherData['days'][0]['tempmax'], WeatherData['days'][0]['tempmin'], WeatherData['days'][0]['temp'],WeatherData['days'][0]['dew'],WeatherData['days'][0]['humidity'], WeatherData['days'][0]['precip'], WeatherData['days'][0]['snow'],WeatherData['days'][0]['snowdepth'],WeatherData['days'][0]['windspeed'],WeatherData['days'][0]['winddir'], WeatherData['days'][0]['pressure'],WeatherData['days'][0]['cloudcover'],WeatherData['days'][0]['visibility']]])
-
-
+      FillInMissingValues(WeatherDataArray)
       
+      WeatherDataArray = FillInMissingValues(WeatherDataArray)
+      WeatherDataArray = WeatherDataArray.astype(str)
+      i = i + 1
+      #if i == 80:
+      #  break
     #Add this data to a csv file so we can join it
     WriteWeatherDatatoCSV(WeatherDataArray)
 
-      
-      
-
-
- 
-
-
-
-#x = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/#timeline/-37.8497,144.968/2012-03-18T06:00:00?key=HAWRLDPJJULW8C79XWHQGZP2F")
-
-
-
-
-
 #Function that gets the data
-#GetCircuitsAndDates()
 GetWeatherForcast(GetCircuitsAndDates())
-
+JoinData()
 
 
